5badabda55b0294812617fbe5c6d66dfc46d9ed6
Kishan Gangwar <142335865+kishn17@users.noreply.github.com>
2024-12-04 23:29:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> computeLPSArray(string& pat) {
+      int n = pat.size();
+      vector<int> lps(n);
+ 
+    int len = 0;
+
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < n) {
+
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+
+        else {
+
+            if (len != 0) {
+                len = lps[len - 1];
+            }
+
+            else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+      return lps;
+}
+    bool areRotations(string &s1, string &s2) {
+        // Your code here
+        
+     string txt = s1 + s1;
+      string pat = s2;
+
+      int n = txt.length();
+    int m = pat.length();
+
+    vector<int> lps = computeLPSArray(pat);
+  
+    int i = 0; 
+    int j = 0;
+    while (i < n) {
+        if (pat[j] == txt[i]) {
+            j++;
+            i++;
+        }
+
+        if (j == m) {
+            return true;
+        }
+
+        else if (i < n && pat[j] != txt[i]) {
+
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+    return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
